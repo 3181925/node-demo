b994714313d5535eecec9dd096c4e8336550ef3d
init <516312423@qq.com>
2017-12-26 13:44:30 +0800

    连接mysql

---
+++ mysql.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var mysql = require('mysql');
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html;  charset=utf-8' });
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '6693166',
+        database: 'test'
+    });
+    connection.connect();
+    connection.query('SELECT* FROM ZZZ', function(err, rows) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(rows);
+            response.write('The title is: ' + JSON.stringify(rows));
+            response.end();
+        }
+
+    });
+}).listen(800);
