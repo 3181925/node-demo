5ba6fa8ed293a78a3c71827e4bf17bee1451200e
init <516312423@qq.com>
2017-12-26 13:39:13 +0800

    暴露函数接口

---
+++ common.js
@@ -0,0 +1,9 @@
+//注意此处使用 module.exports = 函数名 来暴露函数接口
+module.exports = {
+    sayHi: function() {
+        console.log('say hi...')
+    },
+    sayHello: function() {
+        console.log('say hello...')
+    }
+};
